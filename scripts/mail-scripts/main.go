1b653e6dae84550c33756264dc7b110333a98bd1
sidntrivedi012 <sidntrivedi012@gmail.com>
2021-04-30 23:23:30 +0530

    Init code to add unsubscribe option
    Signed-off-by: sidntrivedi012 <sidntrivedi012@gmail.com>

--- scripts/mail-scripts/main.go
+++ scripts/mail-scripts/main.go
@@ -29,6 +29,49 @@ func main() {
 	json.Unmarshal(byteValue, &list)
 
 	send("OwO", list.Subscribers)
+
+    content := "Lorem Ipsum"
+    addUnsubscribe(content string)
+}
+
+// Assuming the HTML rendered string is sent as contentString with the email
+func addUnsubscribeLink(contentString string, email string) {
+
+    // Get the encrypted hash to be sent
+    unsubString := encryptUnsubscribeString(contentString, email)
+    return unsubString
+}
+
+func encryptUnsubscribeString(plainSecret string, email string) string {
+
+	encKey := os.Getenv("EMAIL_ENC_KEY")
+	//Since the key is in string, we need to convert decode it to bytes
+	key, err := hex.DecodeString(encKey)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// convert the string to encrypt to bytes
+	byteSecret := []byte(email)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Creating a new GCM
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Creating a nonce. Nonce should be from GCM
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+
+	ciphertext := aesGCM.Seal(nonce, nonce, byteSecret, nil)
+	return fmt.Sprintf("%x", ciphertext)
 }
 
 func send(body string, to []string) {
